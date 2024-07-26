291d95441e6b6fde5fc59346bfa813ad7bc65f49
kiarraafrica <23-01292@g.batstate-u.edu.ph>
2024-07-26 08:53:12 +0800

    Update main.cpp
    updated

--- main.cpp
+++ main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <string>
+#include <limits>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <conio.h>
+#include <ctime>
+#include <cmath>
+#include <algorithm>
+#include <random>
+#include <cstdlib>
+#include <cstring>
 
+#define ANSI_COLOR_BLUE "\x1b[34m"
+#define ANSI_COLOR_YELLOW "\x1b[33m"
+#define ANSI_COLOR_RED "\033[31m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+using namespace std;
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
